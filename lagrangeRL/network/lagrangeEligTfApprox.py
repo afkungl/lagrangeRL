258e5f53fa86679ac37ae0d034b93c951a04df92
Akos Kungl <fkungl@kip.uni-heidelberg.de>
2018-08-28 18:17:33 +0200

    float64 is standard for the approximative solution as well

--- lagrangeRL/network/lagrangeEligTfApprox.py
+++ lagrangeRL/network/lagrangeEligTfApprox.py
@@ -16,7 +16,7 @@ class lagrangeEligTfApprox(lagrangeEligTf):
         Initialize an empty class instance
         """
 
-        self.dtype = tf.float32
+        self.dtype = tf.float64
         self.T = 0.
 
         # set up a logger
