dab72dba87a165df638993fabc5d47933e714a75
Akos Kungl <fkungl@kip.uni-heidelberg.de>
2018-12-20 17:07:21 +0100

    WIP

--- mlModel/quickAndDirty/spaghettiScript.py
+++ mlModel/quickAndDirty/spaghettiScript.py
@@ -6,7 +6,7 @@ import coloredlogs
 
 
 # Metaparameters
-layers = [100, 100, 3]
+layers = [100, 3]
 labels = [0, 3, 4]
 wInits = []
 for i in range(len(layers) - 1):
@@ -55,49 +55,55 @@ for w in wInits:
     wArrayTf.append(tf.Variable(w, dtype=dtype))
 
 # activities
-activities = []
-for index, w in enumerate(wInits):
-    activities.append(tf.Variable(np.zeros(layers[index + 1]), dtype=dtype))
+#activities = []
+#for index, w in enumerate(wInits):
+#    activities.append(tf.Variable(np.zeros(layers[index + 1]), dtype=dtype))
 
 # action probability
-probs = tf.Variable(np.zeros(layers[-1]), dtype=dtype)
+#probs = tf.Variable(np.zeros(layers[-1]), dtype=dtype)
 
 # Forward pass action selection
-makes = []
+activities = []
 for index, w in enumerate(wArrayTf):
     if index == 0:
-        makes.append(activities[index].assign(tf_mat_vec_dot(w, x)))
+        activities.append(tf.nn.sigmoid(tf_mat_vec_dot(w, x)))
     else:
-        makes.append(activities[index].assign(tf.nn.relu(
-            tf_mat_vec_dot(w, activities[index - 1]))))
+        activities.append(tf.nn.sigmoid(tf_mat_vec_dot(w, activities[index - 1])))
 
 # Update probs
-makes.append(probs.assign(tf.nn.softmax(activities[-1])))
+probs = tf.nn.softmax(activities[-1])
 actionVector = tf.Variable(np.zeros(layers[-1]), dtype=dtype)
 actionIndex = tf.Variable(0,
                           dtype=tf.int64)
-with tf.control_dependencies(makes):
+with tf.control_dependencies(activities + [probs]):
     getAction = actionIndex.assign(tf.multinomial([probs], 1)[0][0])
 cleanActionVector = actionVector.assign(tf.zeros([layers[-1]]))
-makes.append(getAction)
-makes.append(cleanActionVector)
-with tf.control_dependencies(makes):
+with tf.control_dependencies(activities+ [probs, getAction]):
     getActionVector = tf.scatter_update(actionVector, actionIndex, 1.)
 
 
+######################################################
+#### Update the network parameters with gradients ####
 # Modulator
 modulatorTf = tf.placeholder(dtype=dtype,
-                           shape=())
+                             shape=())
+actionVectorPh = tf.placeholder(dtype=dtype, shape=(layers[-1]))
 
-# Update the network parameters with gradients
+# get the gradients
 wgradArr = []
 for wTf in wArrayTf:
-    #wgradArr.append(tf.gradients(tf.pow(tf.reduce_sum(actionVector - probs),2), w, gate_gradients=[actionVector]))
-    wgradArr.append(tf.gradients(probs, wTf))
+    #wgradArr.append(tf.gradients(activities[-1], wTf)[0])
+    wgradArr.append((-1.)*tf.gradients(tf.norm(actionVectorPh - probs), wTf)[0])
+
+# create the update arrays
 updParArray = []
-#error = actionVector - probs
-#for (index, w) in enumerate(wArrayTf):
-#    updParArray.append(w.assign(w + tf_mat_vec_dot(wgradArr[index][0], error)))
+for index, wTf in enumerate(wArrayTf):
+	#updParArray.append(tf.assign(wTf,
+	#	   						 wTf + modulatorTf * tf_mat_vec_dot(wgradArr[index],
+	#	   						 					  actionVector - probs)))
+	with tf.control_dependencies(wgradArr):
+		updParArray.append(tf.assign(wTf,
+									 wTf + learningRate * modulatorTf * wgradArr[index]))
 
 
 '''
@@ -111,43 +117,79 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 # Update the parameters
-def updateParams(modulator):
+def updateParams(modulator, actionVec, inputData):
     for upd in updParArray:
-        sess.run(upd, {modulatorTf: modulator})
+        sess.run(upd, {modulatorTf: modulator,
+        			   actionVectorPh: actionVec,
+        			   x: inputData})
 
 ###########################
 # for the time being: tests
 meanR = 0
 
-for i in range(12):
+for i in range(10000):
 
     print('=============== New Iteration =================')
+    print('Iteration number: {}'.format(i))
+    # clean action
+    sess.run(cleanActionVector)
+
     # obtain input
     inputExample = myData.getRandomTrainExample()[0]
     inputValue = inputExample['data']
 
-    # get action
-    aVec = sess.run(getActionVector, {x: inputValue})
-    action = sess.run(actionIndex)
-    output = sess.run(activities[-1])
-    probabs = sess.run(probs)
-    print('The obtained activity in the last layer is {}'.format(output))
-    print('The obtained probabilities in the last layer is {}'.format(probabs))
-    print('The obtained action in the last layer is {}'.format(action))
-    print('The obtained action vectorin the last layer is {}'.format(aVec))
+    # get action vector
+    aVec = sess.run(getActionVector, {x:inputValue})
+    print('The obtained action vector in the last layer is {}'.format(aVec))
+    print('The desired action vector in the last layer is {}'.format(inputExample['label']))
+
+    # get probabilities
+    probValue = sess.run(probs, {x:inputValue})
+    print('The probability value {}'.format(probValue))
 
     # observe reward
     Reward = rewardScheme.obtainReward(inputExample['label'], aVec)
-    print('The desired action vectorin the last layer is {}'.format(inputExample['label']))
     print('The observed reward is {}'.format(Reward))
 
+    # Update the parameters
+    updateParams(Reward - meanR,
+    			 aVec,
+    			 inputValue)
+
+    # Update the mean reward
+    meanR = gammaDecay * meanR + (1. - gammaDecay) * Reward
+    print('The new mean reward is {}'.format(meanR))
+
+    # wait for user input in the next cycle
+    #input("Press Enter to continue...")
+
+    """
+    # get action
+    probabs = sess.run(probs, {x: inputValue})
+    aVec = sess.run(getActionVector, {x:inputValue})
+    action = sess.run(actionIndex)
+    output = sess.run(activities[-1])
+    print('The obtained activity in the last layer is {}'.format(output))
+    print('The obtained probabilities in the last layer is {}'.format(probabs))
+    print('The obtained action in the last layer is {}'.format(action))
+    print('The obtained action vector in the last layer is {}'.format(aVec))
+    
+
+    # observe reward
+    Reward = rewardScheme.obtainReward(inputExample['label'], aVec)
+    print('The desired action vector in the last layer is {}'.format(inputExample['label']))
+    print('The observed reward is {}'.format(Reward))
+    input("Press Enter to continue...")
+
     # debug gradient
-    grad1 = sess.run(wgradArr[0])
-    print('A calculated gradient is {}'.format(grad1))
+    print(wgradArr[-1])
+    grad1 = sess.run(wgradArr[-1], {x: inputValue})
+    #print('A calculated gradient of wgradArr is {}'.format(grad1))
 
     # Update the parameters
-    updateParams(Reward - np.max([-0.9, meanR]))
+    updateParams(Reward - np.max([-0.9, meanR]), inputValue)
 
     # Update the reward
     meanR = gammaDecay * meanR + (1. - gammaDecay) * Reward
     print('The new mean reward is {}'.format(meanR))
+	"""
