c21297e89d3a07a7578b3fab971eb7419066b9d5
fkungl <fkungl@kip.uni-heidelberg.de>
2019-04-30 18:22:45 +0200

    Add verify backprop experiment to the ML-wna model

--- mlModel/network/mlNetwork.py
+++ mlModel/network/mlNetwork.py
@@ -299,6 +299,10 @@ class mlNetworkWta(mlNetwork):
 
         res = self.sess.run(tensors, inputDict)
 
+        print(res[0])
+        print(res[1])
+        #print(self.sess.run(self.wArrayTf[-1]))
+
         return res[0]
 
     def getUpdateParameters(self, index, wTf):
@@ -378,25 +382,112 @@ class mlNetworkWtaVerifyBp(mlNetworkWta):
 
     """
 
-    def updateParameters(self,
-                         inputVector,
-                         actionVector,
-                         modulator,
-                         learningRate):
+    def _createComputationalGraph(self):
         """
-            Update the parameters based on the formula
+            Create the computational graph ready to start
         """
 
-        for counter, upd in enumerate(self.updParArray):
-            if counter == len(self.updParArray) - 1:
-                # break before updating the last layer
-                break
-            self.sess.run(upd, {self.modulatorTf: modulator,
-                                self.actionVectorPh: actionVector,
-                                self.inputPh: inputVector,
-                                self.learningRateTf: learningRate})
+        # Check if the weights are initialized
+        if not 'wInits' in dir(self):
+            raise RuntimeError("The weights have to be inititalized before creating the computational graph!")
 
-        self.sess.run(self.updateH, {self.modulatorTf: modulator,
-                                self.actionVectorPh: actionVector,
-                                self.inputPh: inputVector,
-                                self.learningRateTf: learningRate})
+        # Input placeholders
+        self.inputPh = tf.placeholder(dtype=self.dtype,
+                                      shape=(self.layers[0]))
+        self.trueLabel = tf.placeholder(dtype=self.dtype,
+                                        shape=(self.layers[-1]))
+        self.meanReward = tf.placeholder(dtype=self.dtype,
+                                         shape=())
+        self.learningRateTf = tf.placeholder(dtype=self.dtype,
+                                             shape=())
+
+        # winner nudges all circuit
+        nNeurons = self.layers[-1]
+        wnaW = np.ones((nNeurons, nNeurons)) * (-1.)/(nNeurons - 1.)
+        np.fill_diagonal(wnaW, 1.) 
+        self.wnaTf = tf.Variable(wnaW, dtype=self.dtype)
+
+        # network array
+        self.wArrayTf = []
+        for w in self.wInits:
+            self.wArrayTf.append(tf.Variable(w, dtype=self.dtype))
+
+        # forward pass of action selection
+        self.activities = []
+        for index, w in enumerate(self.wArrayTf):
+            if index == 0:
+                prevAct = self.inputPh
+            else:
+                prevAct = self.activities[index - 1]
+            if index == len(self.wArrayTf) - 1:
+                #randomCont = tf.random.normal([self.layers[-1]],
+                #                              0.,
+                #                              self.noiseSigma)
+                self.lastLayerU = tfAux.tf_mat_vec_dot(
+                                w,
+                                prevAct) + 5.0 + tf.random.normal(
+                                                    [self.layers[-1]],
+                                                    0.,
+                                                    self.noiseSigma)
+                self.activities.append(
+                        self.actFunc(self.lastLayerU))
+            else:
+                self.activities.append(self.actFunc(tfAux.tf_mat_vec_dot(
+                                                    w,
+                                                    prevAct)))
+
+        # get action vector
+        self.actionVector = tf.Variable(np.zeros(self.layers[-1]),
+                                        dtype=self.dtype)
+        self.actionIndex = tf.Variable(0,
+                                       dtype=tf.int64)
+        self.cleanActionVector = self.actionVector.assign(
+                                        tf.zeros([self.layers[-1]]))
+        with tf.control_dependencies(self.activities + [self.cleanActionVector]):
+            self.getAction = self.actionIndex.assign(
+                                tf.math.argmax(self.activities[-1]))
+
+        with tf.control_dependencies(self.activities+ [self.getAction]):
+            self.getActionVectorTf = tf.scatter_update(self.actionVector,
+                                                     self.actionIndex,
+                                                     1.)
+
+        # obtain reward
+        with tf.control_dependencies([self.getActionVectorTf]):
+            self.R = tf.cond(tf.reduce_all(tf.equal(self.actionVector,
+                                                    self.trueLabel)),
+                             lambda: 1.0,
+                             lambda: -1.0)
+
+        # Set up the parameter updates
+        # Modulator
+        self.modulatorTf = self.R - tf.math.maximum(0., self.meanReward)
+
+        # get the gradients
+        with tf.control_dependencies([self.getActionVectorTf, self.R, self.modulatorTf]):
+            self.wgradArr = []
+            for wTf in self.wArrayTf:
+                self.wgradArr.append(tf.stack(tfAux.jacobian(self.activities[-1],
+                                                         wTf)))
+
+            # do the homoestatic update
+            self.homUpdate = (tf.nn.relu(self.uLow - self.lastLayerU) - tf.nn.relu(self.lastLayerU - self.uHigh))
+            if len(self.layers) == 2:
+                prevAct = self.inputPh
+            else:
+                prevAct = self.activities[-2]
+            self.updateH = self.wArrayTf[-1].assign(self.wArrayTf[-1] + 
+                            self.learningRateH * tfTools.tf_outer_product(
+                                                    self.homUpdate,
+                                                    prevAct))
+
+            # tensors to update the parameters
+            self.updParArray = []
+            for index, wTf in enumerate(self.wArrayTf):
+                if index == len(self.wArrayTf) - 1:
+                    continue
+                self.updParArray.append(self.getUpdateParameters(index, wTf))
+
+        # start the session
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
