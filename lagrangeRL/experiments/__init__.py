48a38bf17ec95779218cad1e92351c3b0ba6d6bf
fkungl <fkungl@kip.uni-heidelberg.de>
2019-09-05 17:18:29 +0200

    Add lagrange based node pert

--- lagrangeRL/experiments/__init__.py
+++ lagrangeRL/experiments/__init__.py
@@ -1,7 +1,8 @@
 experiments = ['expExactLagrange',
                'expExactLagrangeDelay',
                'expExactLagrangeScrambled',
-               'expExactLagrangeVBackprop']
+               'expExactLagrangeVBackprop',
+               'expNodePert']
 
 for experiment in experiments:
     exec('from .{0} import {0}'.format(experiment))
--- lagrangeRL/network/__init__.py
+++ lagrangeRL/network/__init__.py
@@ -7,7 +7,8 @@ networkModels = ['lagrangeElig',
                  'lagrangeFromFirstTest',
                  'lagrangeTfOptimized',
                  'lagrangeTfOptimized2',
-                 'lagrangeTfDirect']
+                 'lagrangeTfDirect',
+                 'lagrangeTfNodePert']
 
 for net in networkModels:
     exec('from {0} import {0}'.format(net))
